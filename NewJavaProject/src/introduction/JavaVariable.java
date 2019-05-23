44fe1b2e835b4e12e6ba4c0dff8497da59573e8f
ankit <ankit@Ankit>
2019-05-22 22:14:00 -0400

    hey

---
+++ NewJavaProject/src/introduction/JavaVariable.java
@@ -0,0 +1,15 @@
+package introduction;
+
+public class JavaVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int TotalLessons = 5;
+int TotalHours= 3;
+int c = TotalLessons + TotalHours;
+String lessons= "software QA lessons"; 
+System.out.print(c+lessons);
+	}
+
+}
